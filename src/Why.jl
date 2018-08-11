948d35afefaac896223df6b980700fd5bd0d9b69
Gaussia <torkel.loman@gmail.com>
2018-08-11 15:15:53 +0100

    Rebalanced some weigths.

--- src/Why.jl
+++ src/Why.jl
@@ -10,7 +10,7 @@ end
 const begins = ("because ", "")
 const fins = ("wanted it", "decided so", "made it happen")
 const mess_persistent = ("Why do you keep asking these questions?", "Please stop.")
-const single = ("I do not know", "42")
+const single = ("I do not know.", "42", "You must find that answer yourself.", "It is axiomatic.")
 function make_ans()
     if (global QUERY_COUNT+=1) > 5 + rand(1:15)
         global QUERY_COUNT -= rand(1:10)
@@ -18,10 +18,23 @@ function make_ans()
     elseif rand() < 0.20
         return message = single[rand(1:length(single))]
     end
-    return uppercasefirst(rand_element(begins) * noun(1.0) * rand_element(fins))
+    return uppercasefirst(rand_element(begins) * noun(1.0) * rand_element(fins) * ".")
 end
 
+#Nouns
+const preps = ("the", "a", "some")
+const nouns = ("compiler", "person", "research software engineer", "type pirate", "cockatoo", "manager", "programmer", "software")
+function noun(p)
+    (rand()>p) && return ""
+    return rand_element(preps)*" "*adj(0.80)*rand_element(nouns)*" "*and(noun(p/2))
+end
 
+#Ajectives
+const adjs = ("fast", "type-safe", "harmless", "deceitful", "cool", "self aware")
+function adj(p)
+    (rand()>p) && return ""
+    return adv(0.5)*rand_element(adjs)*" "*and(adj(p/2.5))
+end
 
 #Adverbs
 const advs = ("vaguely", "extraordinarily", "properly", "very", "not at all")
@@ -30,21 +43,8 @@ function adv(p)
     return rand_element(advs)*" "
 end
 
-#Ajectives
-const adjs = ("fast", "type-safe", "harmless", "deceitful", "cool", "self aware")
-function adj(p)
-    (rand()>p) && return ""
-    return adv(0.66)*rand_element(adjs)*" "*and(adj(p-0.2))
-end
-
-#Nouns
-const preps = ("the", "a", "some")
-const nouns = ("compiler", "person", "research software engineer", "type pirate", "cockatoo", "manager", "programmer", "software")
-function noun(p)
-    (rand()>p) && return ""
-    return rand_element(preps)*" "*adj(0.80)*rand_element(nouns)*" "*and(noun(p-0.3))
-end
-
+.
+#Extra functions
 function and(s::String)
     (s=="") && return s
     return "and " * s
